b311d0942686ca74e292d519dce418caf657aade
D0kL0d <devtemp411@bigmir.net>
2021-07-13 22:02:44 +0300

    Bluetooth trusted devices ooDB

--- app/src/main/java/com/d0klabs/cryptowalt/data/DBHelper.java
+++ app/src/main/java/com/d0klabs/cryptowalt/data/DBHelper.java
@@ -10,7 +10,25 @@ import com.db4o.ObjectSet;
 public class DBHelperQBE {
     public static final String LOG_TAG = DBHelperQBE.class.getSimpleName();
     public static final String DATABASE_NAME = "btCPwal.db";
+    String dbPath;
     ObjectContainer db;
+    boolean openDb(String name) {
+        if (name != null){
+            db = Db4oEmbedded.openFile(Db4oEmbedded.newConfiguration(),DATABASE_NAME);
+            return true;
+        }else return false;
+    }
+    void closeDb(){
+        db.close();
+    }
+    void emptyDb(){
+        ObjectSet result= db.queryByExample(new Object());
+        while(result.hasNext()){
+            db.delete(result.next());
+        }
+    }
+// THX sohaliaziz05
+
     public DBHelperQBE (Context context){
         super (context, "cpwal", null,  1);
     }
