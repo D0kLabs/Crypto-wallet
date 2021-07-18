77fdcc64f6dce0eaedd62d2a8755b5533ba7b862
D0kL0d <devtemp411@bigmir.net>
2021-07-18 23:31:46 +0300

    now need own mBean impl

--- app/src/main/java/com/db4o/cs/monitoring/ClientConnections.java
+++ app/src/main/java/com/db4o/cs/monitoring/ClientConnections.java
@@ -15,9 +15,9 @@ You should have received a copy of the GNU General Public License along
 with this program.  If not, see http://www.gnu.org/licenses/. */
 package com.db4o.cs.monitoring;
 
-import javax.management.*;
+import com.db4o.monitoring.MBeanRegistrationSupport;
 
-import com.db4o.monitoring.*;
+import com.d0klabs.cryptowalt.data.ObjectName;
 
 /**
  * @exclude
