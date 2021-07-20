30d610e8241d5e243960de710bfda876d7da38f0
D0kL0d <devtemp411@bigmir.net>
2021-07-20 16:10:11 +0300

    some savings)

--- app/src/main/java/com/d0klabs/cryptowalt/MainActivity.java
+++ app/src/main/java/com/d0klabs/cryptowalt/MainActivity.java
@@ -32,6 +32,7 @@ import androidx.navigation.ui.NavigationUI;
 import com.d0klabs.cryptowalt.data.DBHelperQBE;
 import com.d0klabs.cryptowalt.databinding.ActivityMainBinding;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Set;
 
@@ -105,6 +106,9 @@ public class MainActivity extends AppCompatActivity {
         }
     });
     private ArrayAdapter<String> discoveredDevicesAdapter;
+
+    public MainActivity() throws IOException {
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/d0klabs/cryptowalt/data/FileIO.java
+++ app/src/main/java/com/d0klabs/cryptowalt/data/FileIO.java
@@ -6,6 +6,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.ByteBuffer;
@@ -13,20 +14,31 @@ import java.nio.ByteBuffer;
 public interface FileIO {
     public final static File FILE_NAME = new File(Environment.getExternalStorageDirectory().toString() + "/content");
     public class OOData {
-        public String sLine;
-        public void createLastData(){}; //
-        public String getLastData (){
+        public static String sLine;
+        public void setLastData(String bBase64encaps) throws IOException {
+            FILE_NAME.setWritable(true);
+            try (FileOutputStream outputStream = new FileOutputStream(FILE_NAME)) {
+                outputStream.write(bBase64encaps.getBytes());
+                outputStream.close();
+                FILE_NAME.setReadOnly();
+            }catch (FileNotFoundException e){
+                getLastData();
+
+            }
+        };
+        public static String getLastData() throws IOException {
            try {
                FileInputStream inputStream = new FileInputStream(FILE_NAME);
                BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
                try {
-                   sLine=br.readLine();
+                   sLine=String.valueOf (br.readLine().toString());
+                   br.close();
                } catch (IOException e) {
                    e.printStackTrace();
                }
                return sLine;
            } catch (FileNotFoundException e){
-               // createLastData : File
+               FILE_NAME.createNewFile();
            }
             return sLine;
         }
