37d758a971afc1013242d687489355f92b2dd25f
D0kL0d <devtemp411@bigmir.net>
2021-07-21 00:57:42 +0300

    refuse bad code

--- app/src/main/java/com/db4o/internal/ObjectContainerFactory.java
+++ app/src/main/java/com/db4o/internal/ObjectContainerFactory.java
@@ -27,20 +27,11 @@ public class ObjectContainerFactory {
 	public static EmbeddedObjectContainer openObjectContainer(EmbeddedConfiguration config) throws OldFormatException {
 		Configuration legacyConfig = Db4oLegacyConfigurationBridge.asLegacy(config);		
 		Config4Impl.assertIsNotTainted(legacyConfig);
-		
-		emitDebugInfo();		
 		EmbeddedObjectContainer oc = new IoAdaptedObjectContainer(legacyConfig, FileIO.FILE_NAME.getName());
 		((EmbeddedConfigurationImpl)config).applyConfigurationItems(oc);
 		Messages.logMsg(legacyConfig, 5, FileIO.FILE_NAME.getName());
 		return oc;
 	}
 
-	private static void emitDebugInfo() {
-	    if (Deploy.debug) {
-			System.out.println("db4o Debug is ON");
-			if (!Deploy.flush) {
-				System.out.println("Debug option set NOT to flush file.");
-			}
-		}
-    }
+
 }
